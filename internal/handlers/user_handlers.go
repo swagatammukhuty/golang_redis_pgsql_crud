38dfde2047dbdc9aede2d99d7ee52dba29d8c722
swagatam <swagatammukhuty@email.com>
2026-06-09 15:57:58 +0530

    Implement create users for redis and postgres

---
+++ internal/handlers/user_handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"golang_redis_pgsql/model"
+	"golang_redis_pgsql/pkg/database"
+	"golang_redis_pgsql/pkg/redis"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	goredis "github.com/redis/go-redis/v9"
+)
+
+var (
+	ctx = context.Background()
+)
+
+func GetAllUsers(c *fiber.Ctx) error {
+	val, err := redis.RedisDB.Get(ctx, "all_users").Result()
+	if err == goredis.Nil {
+		var users []model.User
+		if err := database.DB.Find(&users).Error; err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Postgres DB Error"})
+		}
+		data, _ := json.Marshal(users)
+		redis.RedisDB.Set(ctx, "all_users", data, 10*time.Minute)
+		return c.Status(fiber.StatusOK).JSON(users)
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Redis DB Error"})
+	}
+	var users []model.User
+	if err := json.Unmarshal([]byte(val), &users); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error while Unmarshal"})
+	}
+	return c.Status(fiber.StatusOK).JSON(users)
+}
